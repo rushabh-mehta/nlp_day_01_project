8b371ef32b0d16100f709ed332571e67258e81f2
rushabh-mehta <rushabh123@gmail.com>
2019-01-06 04:52:54 +0000

    Done

--- q02_tokenize/build.py
+++ q02_tokenize/build.py
@@ -1,3 +1,4 @@
+# %load q02_tokenize/build.py
 # Default imports
 
 from nltk.tokenize import TreebankWordTokenizer
@@ -5,5 +6,19 @@ from nltk.tokenize import TreebankWordTokenizer
 import pandas as pd
 
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
-
+path = 'data/20news-bydate-train/'
 # Write your solution here:
+def q02_tokenize(path):
+    file,X_train,X_test,y_train,y_test = q01_load_data(path)
+    tree_bank_tokenizer = TreebankWordTokenizer()
+    X_train = pd.Series(X_train)
+    X_test = pd.Series(X_test)
+    X_train = X_train.apply(lambda x : x.lower())
+    X_test = X_test.apply(lambda x : x.lower())
+    i=0
+    for row in X_train:
+        X_train.iloc[i]=tree_bank_tokenizer.tokenize(str(row))
+        i+=1
+    return X_train
+
+
--- q03_stop_word_stemmer/build.py
+++ q03_stop_word_stemmer/build.py
@@ -1,9 +1,29 @@
+# %load q03_stop_word_stemmer/build.py
 # Default imports
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
+from greyatomlib.nlp_day_01_project.q02_tokenize.build import q02_tokenize
 from nltk.corpus import stopwords
 import pandas as pd
 stop = set(stopwords.words('english'))
 from nltk.stem.porter import PorterStemmer
 
 
+path = 'data/20news-bydate-train/'
 # Your solution here:
+def q03_stop_word_stemmer(path):
+    X_train = q02_tokenize(path)
+    stop_words=set(stopwords.words('english'))
+    porter_stemmer = PorterStemmer()
+    
+    i=0
+    for row in X_train:
+        sentence=[]
+        for word in row:
+            if word not in stop_words:
+                porter_stemmer.stem(word)
+                sentence.append(word)
+        X_train.iloc[i]=sentence
+        i+=1
+    return list(X_train)
+
+
